4f1d920c9cac25c2a61e499d262329b5909bf617
EngenheiroPedroCarvalho <a47113@alunos.isel.pt>
2024-04-16 19:35:50 +0100

    Feito lab03

--- lab03/AP3/AP3.R
+++ lab03/AP3/AP3.R
@@ -3,6 +3,8 @@ library(dplyr) #data manipulation
 library(sparklyr) #spark 
 library(smotefamily) #For SMOTE sampling
 library(data.table) #To be used when possible, as a more performant data.frame
+library(e1071)
+library(caret)
 
 setwd("C:/Users/pedro/Desktop/Mestrado/MDLE/lab/lab03/AP3")
 
@@ -39,11 +41,12 @@ df <- copy_to(sc, df.local)
 #Glimpse of the data set
 #TODO
 sdf <- sdf_schema(df)
-head(sdf)
+head(df)
+actual_rows <- nrow(spark_dataframe(df))
+actual_cols <- ncol(spark_dataframe(df))
 stopifnot(
-  ncol(sdf)==545,
-  nrow(sdf)==2190)
-
+  actual_rows==546,
+  actual_cols==2190)
 
 ################# G3 #######################
 #Feature Selection
@@ -57,7 +60,7 @@ head(df.sel)
 ################# G4 #######################
 #Generating train and test data
 set.seed(123)
-df.split <- df %>% sdf_random_split(training = 0.67, testing = 0.33)
+df.split <- df %>% sdf_random_split(training = 2/3, testing = 1/3)
 df.train <- df.split$training 
 df.test <-  df.split$testing
 
@@ -79,41 +82,133 @@ print(test_class_counts)
 
 random_forest_model <- ml_random_forest(df.train, formula = ("CLASS ~ ."))
 
-mdle.printConfusionMatrix <- function(actual, predicted) {
-  conf_matrix <- table(Actual = actual, Predicted = predicted)
-  print(conf_matrix)
-  accuracy <- sum(diag(conf_matrix)) / sum(conf_matrix)
-  cat("Accuracy:", accuracy, "\n")
-}
-
 # Make predictions on the test data using the trained model
 predictions <- ml_predict(random_forest_model, df.test)
 
-predictions
-# Extract the predicted labels from the predictions
-predicted_labels <- collect(predictions) %>%
-  select(CLASS) %>%
-  as.vector()
-
-# Extract the actual labels from the test data
-actual_labels <- collect(df.test) %>%
-  select(CLASS) %>%
-  as.vector()
-
-# Print confusion matrix and performance metrics
-mdle.printConfusionMatrix(actual_labels, predicted_labels)
+mdle.printConfusionMatrix(predictions, "Random Forest Baseline")
 
 ################# G5 #######################
-#Using imbalanced correcting sampling techniques
-#df.pos.train<- #TODO
-#df.neg.train<- #TODO
-#TODO
+num_pos_instances <- df.train %>%
+  filter(CLASS == 1) %>%
+  sdf_nrow() %>%
+  as.integer()
+num_neg_instances <- df.train %>%
+  filter(CLASS == 0) %>%
+  sdf_nrow() %>%
+  as.integer()
+# Determine the minimum number of instances for balanced sampling
+min_instances <- min(num_pos_instances, num_neg_instances)
+# Undersample the positive and negative classes
+df.pos.train <- df.train %>%
+  filter(CLASS == 1) %>%
+  sdf_sample(fraction = min_instances / num_pos_instances)
+
+df.neg.train <- df.train %>%
+  filter(CLASS == 0) %>%
+  sdf_sample(fraction = min_instances / num_neg_instances)
+
+# Combine the undersampled dataframes
+df.train_balanced <- sdf_bind_rows(df.pos.train, df.neg.train)
+
+# Check the number of instances for each class after undersampling
+num_instances_class_0 <- df.train_balanced %>%
+  filter(CLASS == 0) %>%
+  sdf_nrow() %>%
+  as.integer()
+
+num_instances_class_1 <- df.train_balanced %>%
+  filter(CLASS == 1) %>%
+  sdf_nrow() %>%
+  as.integer()
+
+cat("Number of instances for CLASS=0 after undersampling:", num_instances_class_0, "\n")
+cat("Number of instances for CLASS=1 after undersampling:", num_instances_class_1, "\n")
+
+
+random_forest_model <- ml_random_forest(df.train_balanced, formula = ("CLASS ~ ."))
+# Make predictions on the test data using the trained model
+predictions <- ml_predict(random_forest_model, df.test)
+
+# Print confusion matrix and performance metrics
+mdle.printConfusionMatrix(predictions, "Random Forest Undersampling")
+
+###############################################################################################
 
 #Oversampling
-#TODO
+num_pos_instances <- df.train %>%
+  filter(CLASS == 1) %>%
+  sdf_nrow() %>%
+  as.integer()
+num_neg_instances <- df.train %>%
+  filter(CLASS == 0) %>%
+  sdf_nrow() %>%
+  as.integer()
+# Determine the maximum number of instances for balanced sampling
+max_instances <- max(num_pos_instances, num_neg_instances)
+# Undersample the positive and negative classes
+df.pos.train <- df.train %>%
+  filter(CLASS == 1) %>%
+  sdf_sample(fraction = max_instances / num_pos_instances)
 
+df.neg.train <- df.train %>%
+  filter(CLASS == 0) %>%
+  sdf_sample(fraction = max_instances / num_neg_instances)
+
+# Combine the undersampled dataframes
+df.train_balanced <- sdf_bind_rows(df.pos.train, df.neg.train)
+
+# Check the number of instances for each class after undersampling
+num_instances_class_0 <- df.train_balanced %>%
+  filter(CLASS == 0) %>%
+  sdf_nrow() %>%
+  as.integer()
+
+num_instances_class_1 <- df.train_balanced %>%
+  filter(CLASS == 1) %>%
+  sdf_nrow() %>%
+  as.integer()
+
+cat("Number of instances for CLASS=0 after undersampling:", num_instances_class_0, "\n")
+cat("Number of instances for CLASS=1 after undersampling:", num_instances_class_1, "\n")
+
+random_forest_model <- ml_random_forest(df.train_balanced, formula = ("CLASS ~ ."))
+# Make predictions on the test data using the trained model
+predictions <- ml_predict(random_forest_model, df.test)
+
+# Print confusion matrix and performance metrics
+mdle.printConfusionMatrix(predictions, "Random Forest Oversampling")
+
+#############################################################################################
 #BSMOTE
-#TODO
+# Convert tbl_spark to regular R data frame
+df <- as.data.frame(df.train)
+
+# Extract features (exclude the class label)
+features <- df[, -1]  # Exclude the first column, assuming "CLASS" is the first column
+
+
+# Extract the class labels
+labels <- df$CLASS
+
+# Apply Borderline-SMOTE sampling using BLSMOTE
+
+# Apply BLSMOTE oversampling
+oversampled_data <- BLSMOTE(X = features, target = labels, method = "type1")
+class(oversampled_data)
+
+# Combine oversampled features and labels into a new data frame
+oversampled_df <- data.frame(oversampled_data$data)
+
+class(oversampled_df)
+
+spark_df <- sdf_copy_to(sc, oversampled_df)
+
+random_forest_model <- ml_random_forest(spark_df, formula = ("class ~ ."))
+# Make predictions on the test data using the trained model
+predictions <- ml_predict(random_forest_model, df.test)
+
+# Print confusion matrix and performance metrics
+mdle.printConfusionMatrix(predictions, "Random Forest BLSMOTE")
 
 ################# Spark cleanup ################
 spark_disconnect(sc)
